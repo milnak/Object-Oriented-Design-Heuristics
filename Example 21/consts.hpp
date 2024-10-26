18fcc3669e0bff99c115b10a59e3733214f54ed9
Jeff <33181256+milnak@users.noreply.github.com>
2024-10-26 15:02:53 -0700

    Add consts

--- Example 21/consts.hpp
+++ Example 21/consts.hpp
@@ -1,8 +1,9 @@
+// Consts.hpp: This header file for the ATM system captures a few
+// constant definitions.
 #ifndef CONSTS_HPP
 #define CONSTS_HPP
 
+const unsigned int MaxTransactionAtm = 20;
 const char EnterKey = '\n';
-
-const unsigned int MaxTransactionAtm = 1000;
 
 #endif
